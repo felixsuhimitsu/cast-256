49346ef8396279c01f262a4580d3f19c3e4ce6fd
felixsu <dtc24501007@ictu.edu.vn>
2026-09-08 08:42:14 +0700

    build: tune nextpnr heap placer params for timing closure on Tang Nano 9K

--- scripts/build_yosys.sh
+++ scripts/build_yosys.sh
@@ -21,8 +21,8 @@ nextpnr-himbaechel \
     --vopt family=GW1N-9C \
     --vopt cst=constraints/tangnano9k.cst \
     --sdc constraints/timing.sdc \
-    --placer sa \
-    --placer-sa-effort 3 \
+    --placer-heap-beta 0.98 \
+    --placer-heap-timingweight 5 \
     | tee "${BUILD_DIR}/nextpnr.log"
 
 echo "[3/3] Packing bitstream with Project Apicula..."
